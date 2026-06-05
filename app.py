c05a96375077d53bc09d224fadfe298096b8d8e8
Vinz602 <vinchris602@gmail.com>
2026-06-05 18:35:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -42,8 +42,8 @@ if st.button("Prediksi Status Emosi", type="primary"):
             # Payload JSON menyertakan semua fitur numerik dan kategorikal
             # Pastikan KEY (nama string) di bawah ini SAMA PERSIS dengan nama kolom di DataFrame saat kamu training model
             payload = {
-                "Age": age,
-                "Gender": gender,
+                "age": age,
+                "gender": gender,
                 "Daily_Usage_Time_minutes": daily_usage,
                 "Posts_Per_Day": posts_per_day,
                 "Likes_Received_Per_Day": likes_received,
